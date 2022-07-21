0ed3975d57fece449f71149e5c73c1a9f2ba46ba
EnriqueIzel2 <eleaoizel@gmail.com>
2022-07-21 18:19:32 -0400

    feat: remove Movie initialization

--- app/src/main/java/com/enriqueizel/topmovies/MainActivity.java
+++ app/src/main/java/com/enriqueizel/topmovies/MainActivity.java
@@ -1,21 +1,37 @@
 package com.enriqueizel.topmovies;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.enriqueizel.topmovies.Adapter.AdapterMovie;
+import com.enriqueizel.topmovies.Model.Movie;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
   private RecyclerView recyclerViewMovies;
+  private AdapterMovie adapterMovie;
+  private List<Movie> movieList;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    getComponentsID();
+    movieList = new ArrayList<>();
+    adapterMovie = new AdapterMovie(getApplicationContext(), movieList);
+    recyclerViewMovies.setLayoutManager(new GridLayoutManager(getApplicationContext(), 2));
+    recyclerViewMovies.setHasFixedSize(true);
+    recyclerViewMovies.setAdapter(adapterMovie);
   }
 
-  private void getComponentsID(){
+  private void getComponentsID() {
     recyclerViewMovies = findViewById(R.id.recycler_movies);
   }
 }
