93e89f8ebcfc73c59eafa2ef667739184c5668a0
EnriqueIzel2 <eleaoizel@gmail.com>
2022-07-22 16:59:14 -0400

    feat: set the video controls and the activity orientation

--- app/src/main/java/com/enriqueizel/topmovies/VideoActivity.java
+++ app/src/main/java/com/enriqueizel/topmovies/VideoActivity.java
@@ -2,7 +2,10 @@ package com.enriqueizel.topmovies;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.pm.ActivityInfo;
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.MediaController;
 import android.widget.VideoView;
 
 public class VideoActivity extends AppCompatActivity {
@@ -15,7 +18,15 @@ public class VideoActivity extends AppCompatActivity {
     setContentView(R.layout.activity_video);
     getSupportActionBar().hide();
 
+    setContentView(R.layout.activity_video);
+    this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+
     videoView = findViewById(R.id.video_playing);
     String video = getIntent().getExtras().getString("video");
+
+    videoView.setMediaController(new MediaController(this));
+    videoView.setVideoURI(Uri.parse(video));
+    videoView.requestFocus();
+    videoView.start();
   }
 }
