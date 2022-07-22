99a7287c8580c330658d91d9d198ca4da3389e75
EnriqueIzel2 <eleaoizel@gmail.com>
2022-07-22 16:34:15 -0400

    feat: hide the actionbar

--- app/src/main/java/com/enriqueizel/topmovies/VideoActivity.java
+++ app/src/main/java/com/enriqueizel/topmovies/VideoActivity.java
@@ -3,12 +3,16 @@ package com.enriqueizel.topmovies;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.VideoView;
 
 public class VideoActivity extends AppCompatActivity {
+
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_video);
+    getSupportActionBar().hide();
+
   }
 }
